a9c82623caa98aecf9fccca05dc3ece6ce5c83c2
Ben Muthalaly <benmuthalaly@gmail.com>
2023-12-11 17:52:32 -0600

    Finish part two of DNS in a Weekend
    Can now parse the DNS response and get the IP address.

--- dns.go
+++ dns.go
@@ -3,8 +3,12 @@ package dns
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"math/rand"
+	"net"
+	"strconv"
 	"strings"
 )
 
@@ -17,12 +21,12 @@ import (
 //     num_additionals: int = 0
 
 type DNSHeader struct {
-	id              uint16
-	flags           uint16
-	num_questions   uint16
-	num_answers     uint16
-	num_authorities uint16
-	num_additionals uint16
+	id             uint16
+	flags          uint16
+	numQuestions   uint16
+	numAnswers     uint16
+	numAuthorities uint16
+	numAdditionals uint16
 }
 
 type DNSQuestion struct {
@@ -71,7 +75,7 @@ func buildQuery(domainName string, recordType uint16) ([]byte, error) {
 	}
 	id := uint16(rand.Intn(65535))
 	recursionDesired := uint16(1 << 8)
-	header := DNSHeader{id: id, num_questions: 1, flags: recursionDesired}
+	header := DNSHeader{id: id, numQuestions: 1, flags: recursionDesired}
 	question := DNSQuestion{name: name, type_: recordType, class: CLASS_IN}
 	headerBytes, err := headerToBytes(header)
 	if err != nil {
@@ -84,10 +88,363 @@ func buildQuery(domainName string, recordType uint16) ([]byte, error) {
 	return append(headerBytes, questionBytes...), nil
 }
 
-func main() {
-	fmt.Println("Hello world!")
-	header := DNSHeader{
-		1, 2, 3, 4, 5, 6,
+type DNSRecord struct {
+	// Domain name
+	name []byte
+	// A, AAAA, MX, NS, TXT, etc (encoded as an int)
+	type_ uint16
+	// Always the same for now TODO: look up what this is
+	class uint16
+	// How long to cache the query
+	ttl uint32
+	// The record's content, like the IP Address
+	data []byte
+}
+
+//	func headerToBytes(header DNSHeader) ([]byte, error) {
+//		buf := new(bytes.Buffer)
+//		err := binary.Write(buf, binary.BigEndian, header)
+//		if err != nil {
+//			return nil, err
+//		}
+//		return buf.Bytes(), nil
+//	}
+func parseHeader(response *bytes.Reader) (DNSHeader, error) {
+	const DNSHeaderSize = 12
+	headerBytes := make([]byte, DNSHeaderSize)
+	n, err := response.Read(headerBytes)
+	if err != nil {
+		return DNSHeader{}, err
 	}
-	headerToBytes(header)
+	if n != DNSHeaderSize {
+		return DNSHeader{}, errors.New("not enough data to read the full DNS header")
+	}
+
+	header := DNSHeader{
+		id:             binary.BigEndian.Uint16(headerBytes[:2]),
+		flags:          binary.BigEndian.Uint16(headerBytes[2:4]),
+		numQuestions:   binary.BigEndian.Uint16(headerBytes[4:6]),
+		numAnswers:     binary.BigEndian.Uint16(headerBytes[6:8]),
+		numAuthorities: binary.BigEndian.Uint16(headerBytes[8:10]),
+		numAdditionals: binary.BigEndian.Uint16(headerBytes[10:]),
+	}
+
+	return header, nil
+}
+
+func decodeNameSimple(response *bytes.Reader) (string, error) {
+	fmt.Println("parsing name from response")
+	parts := []string{}
+	length, err := response.ReadByte()
+	if err != nil {
+		return "", err
+	}
+	for length != 0 {
+		// fmt.Printf("length: %v\n", length)
+		nextPart := make([]byte, length)
+		n, err := response.Read(nextPart)
+		if err != nil {
+			return "", err
+		}
+		fmt.Printf("read %v bytes\n", n)
+		fmt.Printf("next part: %s\n", nextPart)
+		parts = append(parts, string(nextPart))
+		length, err = response.ReadByte()
+		if err != nil {
+			return "", err
+		}
+	}
+	return strings.Join(parts, "."), nil
+}
+
+func decodeName(response *bytes.Reader) (string, error) {
+	fmt.Println("parsing name from response")
+	parts := []string{}
+	length, err := response.ReadByte()
+	if err != nil {
+		return "", fmt.Errorf("Failed to read first byte: %w", err)
+	}
+	// fmt.Printf("length: %v\n", length)
+	for length != 0 {
+		fmt.Printf("length: %v\n", length)
+		var nextPart []byte
+		if (length & 0b1100_0000) > 0 {
+			nextPart, err = decodeCompressedName(length, response)
+			if err != nil {
+				return "", fmt.Errorf("Failed to decode compressed name: %w", err)
+			}
+			break
+		} else {
+			nextPart = make([]byte, length)
+			_, err := response.Read(nextPart)
+			if err != nil {
+				return "", fmt.Errorf("Failed to read next part: %w", err)
+			}
+			// fmt.Printf("read %v bytes\n", n)
+		}
+		parts = append(parts, string(nextPart))
+		length, err = response.ReadByte()
+		if err != nil {
+			return "", fmt.Errorf("Failed to read length byte: %w", err)
+		}
+	}
+	return strings.Join(parts, "."), nil
+}
+
+// def decode_compressed_name(length, reader):
+//     pointer_bytes = bytes([length & 0b0011_1111]) + reader.read(1)
+//     pointer = struct.unpack("!H", pointer_bytes)[0]
+//     current_pos = reader.tell()
+//     reader.seek(pointer)
+//     result = decode_name(reader)
+//     reader.seek(current_pos)
+//     return result
+
+func decodeCompressedName(length byte, response *bytes.Reader) ([]byte, error) {
+	fmt.Println("parsing compressed name from response")
+	fmt.Printf("compressed name length: %v\n", length)
+	nextByte, err := response.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("next byte: %0x\n", nextByte)
+	pointerBytes := []byte{length & 0b0011_1111, nextByte}
+	fmt.Printf("pointer bytes: %0 x\n", pointerBytes)
+	pointer := binary.BigEndian.Uint16(pointerBytes)
+	currentPos, err := response.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("current position: %v\n", currentPos)
+	pointerPos, err := response.Seek(int64(pointer), io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("pointer position: %v\n", pointerPos)
+	result, err := decodeName(response)
+	if err != nil {
+		return nil, err
+	}
+	currentPos, err = response.Seek(currentPos, io.SeekStart)
+	fmt.Printf("new current position: %v\n", currentPos)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(result), nil
+}
+
+func parseQuestion(response *bytes.Reader) (DNSQuestion, error) {
+	name, err := decodeName(response)
+	if err != nil {
+		return DNSQuestion{}, err
+	}
+	data := make([]byte, 4)
+	_, err = response.Read(data)
+	if err != nil {
+		return DNSQuestion{}, err
+	}
+
+	return DNSQuestion{
+		name:  []byte(name),
+		type_: binary.BigEndian.Uint16(data[:2]),
+		class: binary.BigEndian.Uint16(data[2:]),
+	}, nil
+}
+
+// def parse_record(reader):
+//     name = decode_name_simple(reader)
+//     # the the type, class, TTL, and data length together are 10 bytes (2 + 2 + 4 + 2 = 10)
+//     # so we read 10 bytes
+//     data = reader.read(10)
+//     # HHIH means 2-byte int, 2-byte-int, 4-byte int, 2-byte int
+//     type_, class_, ttl, data_len = struct.unpack("!HHIH", data)
+//     data = reader.read(data_len)
+//     return DNSRecord(name, type_, class_, ttl, data)
+
+func parseRecord(response *bytes.Reader) (DNSRecord, error) {
+	name, err := decodeName(response)
+	if err != nil {
+		return DNSRecord{}, fmt.Errorf("Failed to decode name: %w", err)
+	}
+	metaData := make([]byte, 10)
+	_, err = response.Read(metaData)
+	if err != nil {
+		return DNSRecord{}, fmt.Errorf("Failed to read metadata: %w", err)
+	}
+	fmt.Printf("metadata: %0 x\n", metaData)
+	dataLen := binary.BigEndian.Uint16(metaData[8:])
+	fmt.Printf("data len: %v\n", dataLen)
+	data := make([]byte, dataLen)
+	n, err := response.Read(data)
+	if err != nil {
+		return DNSRecord{}, fmt.Errorf("Failed to read data: %w", err)
+	}
+	if n != int(dataLen) {
+		return DNSRecord{}, fmt.Errorf("Failed to read correct amount of bytes: expected %v, read %v", dataLen, n)
+	}
+
+	return DNSRecord{
+		name:  []byte(name),
+		type_: binary.BigEndian.Uint16(metaData[:2]),
+		class: binary.BigEndian.Uint16(metaData[2:4]),
+		ttl:   binary.BigEndian.Uint32(metaData[4:8]),
+		data:  data,
+	}, nil
+}
+
+// from typing import List
+
+// @dataclass
+// class DNSPacket:
+//     header: DNSHeader
+//     questions: List[DNSQuestion]
+//     # don't worry about the exact meaning of these 3 record
+//     # sections for now: we'll use them in Part 3
+//     answers: List[DNSRecord]
+//     authorities: List[DNSRecord]
+//     additionals: List[DNSRecord]
+
+type DNSPacket struct {
+	header    DNSHeader
+	questions []DNSQuestion
+	// don't worry about the exact meaning of these 3 record
+	// sections for now: we'll use them in Part 3
+	answers     []DNSRecord
+	authorities []DNSRecord
+	additionals []DNSRecord
+}
+
+type IPAddr []byte
+
+func (ip IPAddr) String() string {
+	parts := make([]string, len(ip))
+	for i, b := range ip {
+		parts[i] = strconv.Itoa(int(b))
+	}
+	str := strings.Join(parts, ".")
+	return str
+}
+
+// def parse_dns_packet(data):
+//     reader = BytesIO(data)
+//     header = parse_header(reader)
+//     questions = [parse_question(reader) for _ in range(header.num_questions)]
+//     answers = [parse_record(reader) for _ in range(header.num_answers)]
+//     authorities = [parse_record(reader) for _ in range(header.num_authorities)]
+//     additionals = [parse_record(reader) for _ in range(header.num_additionals)]
+
+// return DNSPacket(header, questions, answers, authorities, additionals)
+func parseDNSPacket(responseBytes []byte) (DNSPacket, error) {
+	response := bytes.NewReader(responseBytes)
+	header, err := parseHeader(response)
+	if err != nil {
+		return DNSPacket{}, fmt.Errorf("Failed to parse header: %w", err)
+	}
+	fmt.Printf("header: %+v\n", header)
+	questions := make([]DNSQuestion, header.numQuestions)
+	for i := 0; i < len(questions); i++ {
+		q, err := parseQuestion(response)
+		if err != nil {
+			return DNSPacket{}, fmt.Errorf("Failed to parse question: %w", err)
+		}
+		questions[i] = q
+	}
+
+	answers := make([]DNSRecord, header.numAnswers)
+	for i := 0; i < len(answers); i++ {
+		a, err := parseRecord(response)
+		if err != nil {
+			return DNSPacket{}, fmt.Errorf("Failed to parse answer: %w", err)
+		}
+		answers[i] = a
+	}
+
+	authorities := make([]DNSRecord, header.numAuthorities)
+	for i := 0; i < len(authorities); i++ {
+		a, err := parseRecord(response)
+		if err != nil {
+			return DNSPacket{}, fmt.Errorf("Failed to parse authority: %w", err)
+		}
+		authorities[i] = a
+	}
+
+	additionals := make([]DNSRecord, header.numAdditionals)
+	for i := 0; i < len(additionals); i++ {
+		a, err := parseRecord(response)
+		if err != nil {
+			return DNSPacket{}, fmt.Errorf("Failed to parse additional: %w", err)
+		}
+		additionals[i] = a
+	}
+
+	return DNSPacket{
+		header:      header,
+		questions:   questions,
+		answers:     answers,
+		authorities: authorities,
+		additionals: additionals,
+	}, nil
+}
+
+func sendQuery(domain string) ([]byte, error) {
+	UDPAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialUDP("udp", nil, UDPAddr)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	query, err := buildQuery(domain, 1)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Printf("%s\n", query)
+	_, err = conn.Write(query)
+	if err != nil {
+		return nil, err
+	}
+	// fmt.Printf("%v bytes sent\n", n)
+
+	buffer := make([]byte, 1024)
+	n, _, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	// fmt.Println("Received", n, "bytes from", addr)
+	response := buffer[:n]
+	// fmt.Printf("received response: \n%s\n", response)
+	return response, nil
+}
+
+func lookupDomain(domain string) string {
+	response, err := sendQuery(domain)
+	if err != nil {
+		fmt.Printf("Failed to send query: %s\n", err)
+	}
+
+	fmt.Printf("response length: %v\n", len(response))
+	fmt.Printf("response: %0 x\n", response)
+	// fmt.Printf("response domain name: %0 x\n", response[12:30])
+
+	packet, err := parseDNSPacket(response)
+	if err != nil {
+		fmt.Printf("Failed to parse packet: %s\n", err)
+	}
+	fmt.Printf("packet: %+v\n", packet)
+
+	ip := IPAddr(packet.answers[0].data)
+	return ip.String()
+}
+
+func main() {
+	// fmt.Println("Hello world!")
+	// header := DNSHeader{
+	// 	1, 2, 3, 4, 5, 6,
+	// }
+	// headerToBytes(header)
 }
--- dns_test.go
+++ dns_test.go
@@ -1,11 +1,13 @@
 package dns
 
 import (
+	"bytes"
 	"fmt"
-	"net"
 	"reflect"
 	"testing"
 )
+
+const testDomain = "www.example.com"
 
 func TestEncodeDNSName(t *testing.T) {
 	tests := []struct {
@@ -32,36 +34,127 @@ func TestEncodeDNSName(t *testing.T) {
 }
 
 func TestBuildQuery(t *testing.T) {
-	UDPAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+	// TODO: test building the query and not sending a request
+	_, err := sendQuery(testDomain)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Failed to send query. %s\n", err)
+	}
+	// fmt.Println(response)
+}
+
+func TestParseHeader(t *testing.T) {
+	response, err := sendQuery(testDomain)
+	if err != nil {
+		t.Fatalf("Failed to send query. %s\n", err)
+	}
+	reader := bytes.NewReader(response)
+	_, err = parseHeader(reader)
+	// fmt.Printf("header received: %+v\n", header)
+
+}
+
+func TestParseName(t *testing.T) {
+	response, err := sendQuery(testDomain)
+	if err != nil {
+		t.Fatalf("Failed to send query: %s\n", err)
+	}
+	reader := bytes.NewReader(response)
+	_, err = parseHeader(reader)
+	if err != nil {
+		t.Fatalf("Failed to parse header: %s\n", err)
+	}
+	query := make([]byte, 21)
+	_, err = reader.Read(query)
+	if err != nil {
+		t.Fatalf("Failed to parse header: %s\n", err)
+	}
+	// fmt.Printf("%v bytes read from response\n", n)
+	// fmt.Printf("response query: %s\n", query)
+	// fmt.Printf("header received: %+v\n", header)
+}
+
+func TestParseQuestion(t *testing.T) {
+	response, err := sendQuery(testDomain)
+	if err != nil {
+		t.Fatalf("Failed to send query: %s\n", err)
+	}
+	fmt.Printf("response: %0 x\n", response)
+	responseReader := bytes.NewReader(response)
+
+	_, err = parseHeader(responseReader)
+	if err != nil {
+		t.Fatalf("Failed to parse header: %s\n", err)
 	}
 
-	conn, err := net.DialUDP("udp", nil, UDPAddr)
+	question, err := parseQuestion(responseReader)
+	fmt.Printf("domain name: %s\n", question.name)
+	fmt.Printf("question: %+v\n", question)
+	query := make([]byte, 16)
+	n, err := responseReader.Read(query)
 	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
-
-	query, err := buildQuery("www.example.com", 1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Printf("%s\n", query)
-	n, err := conn.Write(query)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%v bytes sent", n)
-
-	buffer := make([]byte, 1024)
-	n, addr, err := conn.ReadFromUDP(buffer)
-	if err != nil {
-		fmt.Println(err)
-		return
+		t.Fatalf("Failed to parse header: %s\n", err)
 	}
 
-	fmt.Println("Received", n, "bytes from", addr)
-	response := buffer[:n]
-	fmt.Printf("received response: \n%s\n", response)
+	fmt.Printf("%v bytes read from response\n", n)
+	fmt.Printf("response query: %0 x\n", query)
+}
+
+func TestParseRecord(t *testing.T) {
+	response, err := sendQuery(testDomain)
+	if err != nil {
+		t.Fatalf("Failed to send query: %s\n", err)
+	}
+	fmt.Printf("response: %0 x\n", response)
+	reader := bytes.NewReader(response)
+
+	_, err = parseHeader(reader)
+	if err != nil {
+		t.Fatalf("Failed to parse header: %s\n", err)
+	}
+
+	_, err = parseQuestion(reader)
+	if err != nil {
+		t.Fatalf("Failed to parse question: %s\n", err)
+	}
+
+	record, err := parseRecord(reader)
+	if err != nil {
+		t.Fatalf("Failed to parse record: %s\n", err)
+	}
+
+	fmt.Printf("domain name: %s\n", record.name)
+	fmt.Printf("data: %0x\n", record.data)
+	fmt.Printf("record: %+v\n", record)
+}
+
+func TestParsePacket(t *testing.T) {
+	response, err := sendQuery(testDomain)
+	if err != nil {
+		t.Fatalf("Failed to send query: %s\n", err)
+	}
+
+	fmt.Printf("response length: %v\n", len(response))
+	fmt.Printf("response: %0 x\n", response)
+	// fmt.Printf("response domain name: %0 x\n", response[12:30])
+
+	packet, err := parseDNSPacket(response)
+	if err != nil {
+		t.Fatalf("Failed to parse packet: %s\n", err)
+	}
+	fmt.Printf("packet: %+v\n", packet)
+
+	ip := IPAddr(packet.answers[0].data)
+	fmt.Printf("packet data: %s\n", ip)
+
+	// fmt.Printf("%0x\n", 0b1100_0000)
+	// fmt.Printf("%0x\n", 0b0011_1111)
+}
+
+func TestLookupDomain(t *testing.T) {
+	ip := lookupDomain("www.example.com")
+	fmt.Printf("ip: %s\n", ip)
+	ip = lookupDomain("www.recurse.com")
+	fmt.Printf("ip: %s\n", ip)
+	ip = lookupDomain("www.metafilter.com")
+	fmt.Printf("ip: %s\n", ip)
 }
